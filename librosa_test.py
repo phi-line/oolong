33a845b8d1e76c090a38294c058bc5a0d1fc485c
phi-line <phi.liney@gmail.com>
2017-10-20 16:40:55 -0700

    reverted to old script

--- librosa_test.py
+++ librosa_test.py
@@ -1,93 +1,61 @@
 from __future__ import print_function
 
-import sys  # kwargs
+import sys #kwargs
+
+import numpy as np
+import scipy as sp
+# import keras only if we need DNN
+import matplotlib.pyplot as plt
 
 # Librosa is a music analysis tool
 # https://github.com/librosa/librosa
 import librosa
 import librosa.display
-# import keras only if we need DNN
-import matplotlib.pyplot as plt
-import numpy as np
 
-# Compute local onset autocorrelation
-y, sr = librosa.load('Song2.mp3')
-hop_length = 512
-oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length)
-tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr,
-                                      hop_length=hop_length)
-# Compute global onset autocorrelation
-ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0])
-ac_global = librosa.util.normalize(ac_global)
-# Estimate the global tempo for display purposes
-tempo = librosa.beat.tempo(onset_envelope=oenv, sr=sr,
-                           hop_length=hop_length)[0]
+def main():
+    if len(sys.argv) < 3:
+        print ('Error: invalid number of arguments')
+        print ('Usage: spectrogram.py type song.mp3')
+        print ('Types: [mel] ')
+        sys.exit()
+    else:
+        if sys.argv[1] == 'mel':
+            mel_spectrogram(sys.argv[2], display=True)
 
-import matplotlib.pyplot as plt
-plt.figure(figsize=(8, 8))
-# We'll truncate the display to a narrower range of tempi
-plt.axhline(tempo, color='w', linestyle='--', alpha=1,
-            label='Estimated tempo={:g}'.format(tempo))
-plt.legend(frameon=True, framealpha=0.75)
-plt.subplot(4, 1, 1)
-x = np.linspace(0, tempogram.shape[0] * float(hop_length) / sr,
-                num=tempogram.shape[0])
-plt.plot(x, np.mean(tempogram, axis=1), label='Mean local autocorrelation')
-plt.plot(x, ac_global, '--', alpha=0.75, label='Global autocorrelation')
-plt.xlabel('Lag (seconds)')
-plt.axis('tight')
-plt.legend(frameon=True)
-plt.subplot(4,1,2)
-# We can also plot on a BPM axis
-freqs = librosa.tempo_frequencies(tempogram.shape[0], hop_length=hop_length, sr=sr)
-plt.semilogx(freqs[1:], np.mean(tempogram[1:], axis=1),
-             label='Mean local autocorrelation', basex=2)
-plt.semilogx(freqs[1:], ac_global[1:], '--', alpha=0.75,
-             label='Global autocorrelation', basex=2)
-plt.axvline(tempo, color='black', linestyle='--', alpha=.8,
-            label='Estimated tempo={:g}'.format(tempo))
-plt.legend(frameon=True)
-plt.xlabel('BPM')
-plt.axis('tight')
-plt.grid()
-plt.tight_layout()
-################################################################
-y, sr = librosa.load('Song3.mp3')
-hop_length = 512
-oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length)
-tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr,
-                                      hop_length=hop_length)
-# Compute global onset autocorrelation
-ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0])
-ac_global = librosa.util.normalize(ac_global)
-# Estimate the global tempo for display purposes
-tempo = librosa.beat.tempo(onset_envelope=oenv, sr=sr,
-                           hop_length=hop_length)[0]
-# We'll truncate the display to a narrower range of tempi
-plt.axhline(tempo, color='w', linestyle='--', alpha=1,
-            label='Estimated tempo={:g}'.format(tempo))
-plt.legend(frameon=True, framealpha=0.75)
-plt.subplot(4, 1, 3)
-x = np.linspace(0, tempogram.shape[0] * float(hop_length) / sr,
-                num=tempogram.shape[0])
-plt.plot(x, np.mean(tempogram, axis=1), label='Mean local autocorrelation')
-plt.plot(x, ac_global, '--', alpha=0.75, label='Global autocorrelation')
-plt.xlabel('Lag (seconds)')
-plt.axis('tight')
-plt.legend(frameon=True)
-plt.subplot(4,1,4)
-# We can also plot on a BPM axis
-freqs = librosa.tempo_frequencies(tempogram.shape[0], hop_length=hop_length, sr=sr)
-plt.semilogx(freqs[1:], np.mean(tempogram[1:], axis=1),
-             label='Mean local autocorrelation', basex=2)
-plt.semilogx(freqs[1:], ac_global[1:], '--', alpha=0.75,
-             label='Global autocorrelation', basex=2)
-plt.axvline(tempo, color='black', linestyle='--', alpha=.8,
-            label='Estimated tempo={:g}'.format(tempo))
-plt.legend(frameon=True)
-plt.xlabel('BPM')
-plt.axis('tight')
-plt.grid()
-plt.tight_layout()
 
-plt.show()
+def mel_spectrogram(mp3 = sys.argv[2], display = True):
+    '''
+    this function displays a mel spectrogram .csv of the mp3 data
+    :return:
+    '''
+    if len(sys.argv) >= 1:
+        y, sr = librosa.load(path=mp3)
+
+    # Let's make and display a mel-scaled power (energy-squared) spectrogram
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+
+    # Convert to log scale (dB). We'll use the peak power as reference.
+    log_S = librosa.logamplitude(S, ref_power=np.max)
+
+    # Make a new figure
+    plt.figure(figsize=(12,4))
+
+    # Display the spectrogram on a mel scale
+    # sample rate and hop length parameters are used to render the time axis
+    librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
+
+    # Put a descriptive title on the plot
+    plt.title('mel power spectrogram')
+
+    # draw a color bar
+    plt.colorbar(format='%+02.0f dB')
+
+    # Make the figure layout compact
+    plt.tight_layout()
+
+    # display
+    if display:
+        plt.show()
+
+if __name__ == '__main__':
+    main()
