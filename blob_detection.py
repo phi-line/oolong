b11ed71048b3a250b8bc47c8e347458241554745
phi-line <phi.liney@gmail.com>
2017-11-01 02:34:40 -0700

    removed flann and brute from feature gathering

--- blob_detection.py
+++ blob_detection.py
@@ -41,8 +41,6 @@ def main():
     original = imsave(image, rgb2gray(mel_original))
 
     feat_censure(mel_slice, mel_original)
-    # brute(slice, original)
-    # flann(slice, original)
 
 def feat_censure(slice, original):
     from skimage.feature import CENSURE
@@ -53,69 +51,11 @@ def feat_censure(slice, original):
 
     detector.detect(slice)
     kp = detector.keypoints
-    # hist_feature(detector.keypoints, slice)
     xx, yy, zz = kd_feature(kp, 4.0, metric='manhattan')
 
     plt.pcolormesh(xx, yy, zz, cmap=plt.cm.gist_heat)
     plt.scatter(x=kp[:, 1], y=kp[:, 0], s=2 ** detector.scales, facecolor='white', alpha=.5)
     plt.axis('off')
-    plt.show()
-
-def flann(slice, original):
-    import cv2
-    from matplotlib import pyplot as plt
-
-    img1 = cv2.cvtColor(cv2.imread(slice), cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(cv2.imread(original), cv2.COLOR_BGR2GRAY)
-
-    # Initiate SIFT detector
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
-
-    # FLANN parameters
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)  # or pass empty dictionary
-
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-    matches = flann.knnMatch(des1, des2, k=2)
-
-    # Need to draw only good matches, so create a mask
-    matchesMask = [[0, 0] for i in range(len(matches))]
-
-    # ratio test as per Lowe's paper
-    for i, (m, n) in enumerate(matches):
-        if m.distance < 0.7 * n.distance:
-            matchesMask[i] = [1, 0]
-
-    draw_params = dict(matchColor=(0, 255, 0),
-                       singlePointColor=(255, 0, 0),
-                       matchesMask=matchesMask,
-                       flags=0)
-
-    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
-
-    plt.imshow(img3, ), plt.show()
-
-def hist_feature(scatter, slice):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from scipy.stats import gaussian_kde
-    x = scatter[:, 1]; y = scatter[:, 0]
-
-    # Calculate the point density
-    xy = np.vstack([x, y])
-    z = gaussian_kde(xy)(xy)
-
-    # Sort the points by density, so that the densest points are plotted last
-    idx = z.argsort()
-    x, y, z = x[idx], y[idx], z[idx]
-
-    plt.scatter(x, y, c=z)
     plt.show()
 
 def kd_feature(scatter, bandwidth, xbins=100j, ybins=100j, **kwargs):
